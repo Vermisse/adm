e6f3a7fe1cf9a061db7a2fd51df926bdb36243e6
vermisse <vermisse@qq.com>
2017-08-02 12:58:09 +0800

    新样式

--- src/main/java/com/soft/web/dao/product/ProductMapper.java
+++ src/main/java/com/soft/web/dao/product/ProductMapper.java
@@ -1,6 +1,13 @@
 package com.soft.web.dao.product;
 
+import java.util.*;
+
+import org.apache.ibatis.annotations.*;
+
 public interface ProductMapper {
 
-	
+
+	List<Map> queryProduct(@Param("product_name") String product_name,
+			@Param("product_price") Double product_price,
+			@Param("state") String state);
 }
